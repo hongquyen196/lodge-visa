ad80f510588e007a4445c481cea3f89413dd58ae
Le Hong Quyen <hongquyen196@gmail.com>
2022-10-27 09:42:52 +0800

    update run.sh for production

--- notification/run.sh
+++ notification/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Development
-node src/index.js
+# node src/index.js
 # Production
-# node /var/www/html/lodge-visa/notification/src/index.js
+node /var/www/html/lodge-visa/notification/src/index.js

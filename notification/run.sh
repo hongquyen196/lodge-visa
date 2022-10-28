2258bfd1a9015b30a76d78cc0e652ebc2d7128a5
Le Hong Quyen <hongquyen196@gmail.com>
2022-10-28 16:25:12 +0800

    fix run.sh for crontab on production

--- notification/run.sh
+++ notification/run.sh
@@ -2,4 +2,5 @@
 # Development
 # node src/index.js
 # Production
-node /var/www/html/lodge-visa/notification/src/index.js
+# node /var/www/html/lodge-visa/notification/src/index.js
+cd /var/www/html/lodge-visa/notification && /root/.nvm/versions/node/v16.18.0/bin/node src/index.js
